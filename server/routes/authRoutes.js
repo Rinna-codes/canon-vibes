cdec5b19401ae897ceda4ff67a05ca4abe98df0c
Zharinna Subido <zbsxph@mail.umkc.edu>
2026-04-13 21:05:22 -0500

    create the routers for the login and register requests to go to controller file.

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,1 +1,13 @@
-// maps the create account/register and login URLs to the right controller functions in controllers folder
+// maps or routes the create account/register and login URLs to the right controller functions in controllers folder
+
+const express = require('express');
+const authControler = require('../controllers/authController'); // finds where the the controller file is at
+const router = express.Router();
+
+// user is trying to create an account, catches the request and goes to the registerUser function in authController file
+router.post('/register', authControler.registerUser); 
+
+// user is trying to login into account, catches the request and goes to the loginUser function in authController file
+router.post('/login', authControler.loginUser);
+
+module.exports = router;
