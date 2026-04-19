9d6a3a0021370f2917e849ae25470fd5a234a9e9
Zharinna Subido <zbsxph@mail.umkc.edu>
2026-04-18 21:51:55 -0500

    Set up react routers to the correct pages of app for login, register, dashboard, and soundtrack card pages.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,121 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+// contains the routers for each separeate web page
+
+// BrowserRouter — wraps your whole app and enables routing
+// Routes — a container that holds all your route definitions
+// Route — defines one specific path and which component to show
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+// Need to import the pages into the app component
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/Dashboard';
+import RegisterPage from './pages/RegisterPage';
+import SoundtrackCardPage from './pages/SoundtrackCardPage';
 
 function App() {
-  const [count, setCount] = useState(0)
+  return(
+    <BrowserRouter>
+      <Routes>
+        {/* This makes the router to the user Login Page */}
+        <Route path="/login" element={<LoginPage />} /> 
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+        {/* This makes the router to the users DashboardPage */}
+        <Route path="/dashboard" element={<DashboardPage />} />
 
-      <div className="ticks"></div>
+        {/* This makes the rotuer to the user Register Page */}
+        <Route path="/register" element={<RegisterPage/>} />
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+        {/* This makes the router to a users Soundtrack Card Page */}
+        <Route path="/soundtrackCard" element={<SoundtrackCardPage />} />
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/SoundtrackCardPage.jsx
@@ -0,0 +1,9 @@
+function SoundtrackCardPage() {
+    return (
+        <div>
+            <h1>Soundtrack Card Page</h1>
+        </div>
+    );
+};
+
+export default SoundtrackCardPage;
