5759f9dda09491394bde6034e081137620365d30
Zharinna Subido <zbsxph@mail.umkc.edu>
2026-04-22 21:58:04 -0500

    created and tested in character search query as part of the card creation page form.

--- client/src/pages/CreateCardPage.jsx
+++ client/src/pages/CreateCardPage.jsx
@@ -19,6 +19,49 @@ function CreateCardForm() {
         moodBoard: []
     });
 
+    const [searchQuery, setSearchQuery] = useState(''); // saves the users search query for a charater
+    const [searchResult, setSearchResult] = useState([]); // saves the array of characters from the users search query
+
+    const handleSearch = async (e) => {
+        // fetches the character/search query with the token at the header
+        // store the results in the searchResult list 
+        e.preventDefault(); // prevents the web page from reloading when called 
+
+        try {
+            const response = await fetch(`/api/characters/search?name=${searchQuery}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type' : 'application/json'
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not OKAY');
+            }
+
+            const data = await response.json();
+            setSearchResult(data); // saves the several results into the list
+        } catch (error) {
+            console.error('Error in fetching the search results, sorry! 😔');
+        }
+    };
+
+    const handleSelectCharacter = (character) => {
+        setFormData(prev => ({
+            ...prev, // spread the exisiting state, keeping everything already in formData
+
+            // but replace these specific fields will update when user searches for character
+            superheroName: character.name,
+            characterRealName: character.real_name,
+            comicVineID: character.id,
+            characterImage: character.image.medium_url,
+            // TODO: update in adding the rest of the form fields 
+        }));
+
+        setSearchResult([]);
+        console.log(formData);
+    };
+
     const handleChange = (e) => { // genaric object handler for all fields
         const {name, value} = e.target; // follows "formData, setFormData"
         setFormData({...formData, [name] : value}); // [name] is dynamic, use whichever field name is updated ONLY
@@ -27,9 +70,22 @@ function CreateCardForm() {
     const navigate = useNavigate()
 
     return (
-        <diiv>
-            <h1>Card Creation Completed!</h1>
-        </diiv>
+        <div>
+            <input
+            type="text"
+            value = {searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            placeholder="Search characers..."
+            />
+            <button onClick={handleSearch}>Search</button>
+
+            {/* Shows the result from query search */}
+            <ul>
+                {searchResult.map(character => (
+                    <li key={character.id} onClick={() => handleSelectCharacter(character)}>{character.name}</li>
+                ))}
+            </ul>
+        </div>
     );
 }
 
