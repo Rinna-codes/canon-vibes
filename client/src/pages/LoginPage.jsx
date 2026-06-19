02b9901321c67a00f11234383473830347adc1dc
Zharinna Subido <zbsxph@mail.umkc.edu>
2026-06-18 21:45:08 -0500

    added tailwindcss stylization: first time learning in creating the login card form with tailwindcss in the form of utilizing in creating an object for reability.

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -39,32 +39,47 @@ const LoginPage = () => {
         }
     };
 
+    const classes = {
+        pageBody: 'h-screen flex bg-gray-bg1',
+        formContainer:
+            'w-full max-w-md m-auto bg-white rounded-lg border border-primaryBorder shadow-default py-10 px-16',
+        formHeading: 'text-2xl  font-medium text-primary mt-4 mb-12 text-center',
+        btnContainer: 'flex justify-center items-center mt-6'};
+
     return (
-        <form onSubmit={handleSubmit}>
-        <div>
-            {/* <label htmlFor='email'>Email Address</label> for putting Email Address label above input */}
-            <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder='Email Address'/>
-        </div>
+        <div className='continer-wrapper'>
+            <div className={classes.pageBody}>
+                <div className={classes.formContainer}>
+                    <h1 className={classes.formHeading}> Login into your account </h1>
 
-        <div>
-            <input
-            type="password"
-            value={password}
-            onChange={(p) => setPassword(p.target.value)}
-            placeholder='Password'
-            />
-        </div>
-        
-        <Link to="/register">Don't have an account? Register here</Link>
+                    <form onSubmit={handleSubmit}>
+                    <div>
+                        {/* <label htmlFor='email'>Email Address</label> for putting Email Address label above the input field */}
+                        <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder='Email Address'/>
+                    </div>
 
-        <div>
-            <button type="submit">Submit</button>
+                    <div>
+                        <input
+                        type="password"
+                        value={password}
+                        onChange={(p) => setPassword(p.target.value)}
+                        placeholder='Password'
+                        />
+                    </div>
+                    
+                    <Link to="/register">Don't have an account? Register here</Link>
+
+                    <div className={classes.btnContainer}>
+                        <button type="submit">Submit</button>
+                    </div>
+                    </form>
+                </div>
+            </div>
         </div>
-        </form>
     );
 };
 
