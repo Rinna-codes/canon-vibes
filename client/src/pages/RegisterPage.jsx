fd861aa30ce6fbe64fef5724dcf567e6649ebe20
Zharinna Subido <zbsxph@mail.umkc.edu>
2026-06-22 17:57:31 -0500

    started on stylization for the register page.

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -34,32 +34,28 @@ const RegisterPage = () => {
         }
     };
 
+    const pageForm = {
+        webPageGrid: 'bg-white-900 py-12 sm:py-16',
+        panelContainer:'mx-auto max-w-7xl px-6 lg:px-8',
+        panelGrid: 'grid grid-cols-1 gap-6 lg:grid-cols-6'
+    };
+
+    const comicPanels = {
+        topLeftPanel: 'relative flex min-h-45 flex-col items-center justify-center rounded-lg bg-[#CDB4DB] border-5 border-black px-8 py-10 lg:col-span-3',
+        topRightPanel: 'flex min-h-45 flex-col items-center justify-center gap-4 rounded-lg bg-[#A2D2FF] border-5 border-black px-8 py-10 sm:flex-row lg:col-span-3',
+        mainSection: 'flex flex-col lg:flex-row items-center rounded-lg bg-[#FFC8DD] border-5 border-black gap-20 px-8 py-10 sm:px-10 lg:col-span-6',
+        bottomLeftPanel: 'min-h-40 rounded-lg bg-[#A2D2FF] border-5 border-black px-8 py-8 lg:col-span-2',
+        bottomMiddlePanel: 'min-h-40 rounded-lg bg-[#FFAFCC] border-5 border-black px-8 py-8 lg:col-span-2',
+        bottomrRightPanel: 'flex min-h-40 flex-col bg-[#BDE0FE] items-center justify-center rounded-lg border-5 border-black px-8 py-8 text-center lg:col-span-2'
+    };
+
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder='Username'
-            />
-
-            <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder='Email'
-            />
-
-            <input
-            type="password"
-            value={password}
-            onChange={(p) => setPassword(p.target.value)}
-            placeholder='Password'
-            />
-            <button>Submit</button>
-
-            <Link to="/login">Have an account already? Login here!</Link>
-        </form>
+        <div className={pageForm.webPageGrid}>
+            <div className={pageForm.panelContainer}>
+                {/* top left panal, Canon Vibes titles */}
+                
+            </div>
+        </div>
     );
 };
 
