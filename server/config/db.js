2482f49b6c408f69edb8c170252a54f3a4f9037e
Zharinna Subido <zbsxph@mail.umkc.edu>
2026-08-16 20:32:58 -0400

    review: edited and reviewed content of the authentication controller file.

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,10 @@
-const mongoResponse = require('mongoose'); // requires mongoose 
+const mongoResponse = require('mongoose'); // requires mongoose to use
 
  async function connectToDatabase() {
     const uri = process.env.MONGODB_URI; // retrieve the MongoDB conn. string in .env file 
 
     try {
-        const response = await mongoResponse.connect(uri); // connect the database through the conn. string 
+        const response = await mongoResponse.connect(uri); // connect the database through the string 
         console.log("Connection to MongoDB was successful!"); 
     } catch(error) {
         console.error("Connection to MongoDB failed:", error);
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,28 +1,28 @@
 // handles the functions and logic for registering + logging in (aka authentication)
 
-const bcrypt = require('bcrypt'); // need for hashing passwords (security purposes)
+const bcrypt = require('bcrypt'); // need for hashing the passwords (for security purposes)
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const registerUser = async (req, res) => {
-    // takes the new credentials from the clients requests
-    // check if the user already exist
-    // hashes the password
-    // save the new user to the MongoDB
-    // send a response back to client/user
+    // takes the new credentials from the client requests
+    // checks if the user already exists
+    // hashes the user's password
+    // save the new user to the MongoDB database
+    // send a response back to client/user for acnknowledgement for user registration
 
     try {
         const {username, email, password} = req.body; 
         const existingUser = await User.findOne({email});
 
-        if (existingUser) { 
-            return res.status(400).json({error: 'Email already in used for an existing user'});
+        if (existingUser) {
+            return res.status(400).json({error: 'Email found in used for an existing user'});
         }
 
         const hashedPassword = await bcrypt.hash(password, 10); // hash the password with 10 salt rounds
         const saveNewUser = await User.create({username: username, email: email, password: hashedPassword});
 
-        res.status(201).json({message: 'New Soundtrack Card User Created! Yay!'});
+        res.status(201).json({message: 'New Soundtrack Card User Created!'});
     } catch (err) {
         console.error(err); 
         res.status(500).json({message:"Whoops! Something went wrong with registering 😱"});
@@ -30,10 +30,10 @@ const registerUser = async (req, res) => {
 };
 
 const loginUser = async (req, res) => {
-    // extract the email and password from clients requests 
-    // find the email in the mongo database for existing user, if nothing gets back respond with error message 
+    // extract the email and password from client requests 
+    // find the email in the database for existing user, if nothing, respond with error message 
     // compare the requested password to verify if in database, otherwise send back an error message if not found 
-    // when both successful in verification, send/respond with the jwt token  
+    // when both successful in verification, send/respond with the jwt token to authenticated user  
 
     try {
         const {email, password} = req.body;
@@ -46,17 +46,17 @@ const loginUser = async (req, res) => {
         const comparePwd = await bcrypt.compare(password, existingEmail.password)
 
         if (!comparePwd) { 
-            return res.status(400).json({error: 'There is no password found'});
+            return res.status(400).json({error: 'Incorrect password'});
         }
 
-        // generate the json web token 
+        // generate the json web token for user authentication
         const token = jwt.sign(
             {id: existingEmail._id}, // payload 
             process.env.JWT_SECRET, // secret jwt key 
             {expiresIn: '7d' }
         )
 
-        res.status(200).json({token, username: existingEmail.username}); // returns token for successful token to authenticated user
+        res.status(200).json({token, username: existingEmail.username});
 
     } catch (err) {
         console.error(err);
